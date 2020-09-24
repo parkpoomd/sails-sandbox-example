9463730caf632e89e478d012b51c3c8477b994b6
parkpoomd <parkpoom.d@gmail.com>
2020-09-24 16:14:13 +0700

    up projects on github

---
+++ api/controllers/PostController.js
@@ -0,0 +1,45 @@
+module.exports = {
+  posts: async function(req, res) {
+    try {
+      const posts = await Post.find();
+      res.send(posts);
+    } catch (err) {
+      res.serverError(err.toString());
+    }
+  },
+  create: function(req, res) {
+    const title = req.body.title;
+    const postBody = req.body.postBody;
+
+    Post.create({ title: title, body: postBody }).exec(err => {
+      if (err) {
+        return res.serverError(err.toString());
+      }
+
+      console.log('Finished creating post object');
+      return res.redirect('/home');
+      //   return res.end();
+    });
+  },
+  findById: function(req, res) {
+    const postId = req.param('postId');
+
+    const filteredPosts = allPosts.filter(p => {
+      return p.id == postId;
+    });
+
+    if (filteredPosts.length > 0) {
+      res.send(filteredPosts[0]);
+    } else {
+      res.send('Failed to find post by id: ' + postId);
+    }
+  },
+  delete: async function(req, res) {
+    const postId = req.param('postId');
+    console.log(postId);
+
+    await Post.destroy({ id: postId });
+
+    res.send('Finished deleting post');
+  }
+};
